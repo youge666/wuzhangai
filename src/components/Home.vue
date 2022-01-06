7ac1f7acc78a617340503f09ec5bd62e1f175dfb
Huyou <780029744@qq.com>
2022-01-06 21:26:00 +0800

    完成了登录功能

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,21 @@
 <template>
-<div>hello</div>
+<div>
+  <el-button type="info" @click="logout">退出</el-button>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {}
+  },
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -23,4 +23,19 @@ const router = new VueRouter({
   routes
 })
 
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  // next() 直接放行   next('/login') 强制跳转到'/login'
+
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  console.log(tokenStr)
+  if (!tokenStr) return next('/login')
+  next()
+})
+
 export default router
